bcc9dbebcd72873dd0934104c8f7e27c295093f2
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2022-02-17 01:21:41 -0500

    Initial Commit

---
+++ app/src/main/java/com/saurav/mapd_booktracker_a00236910/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saurav.mapd_booktracker_a00236910;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
