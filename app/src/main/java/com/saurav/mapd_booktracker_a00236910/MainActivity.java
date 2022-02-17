330a019a9ce7c9b5a0f7358e19db352104ce8c73
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2022-02-17 02:24:02 -0500

    Splash screen added and login activity and layout resource file added

--- app/src/main/java/com/saurav/mapd_booktracker_a00236910/MainActivity.java
+++ app/src/main/java/com/saurav/mapd_booktracker_a00236910/MainActivity.java
@@ -2,13 +2,36 @@ package com.saurav.mapd_booktracker_a00236910;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH = 3300;
+
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.text);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
     }
 }
